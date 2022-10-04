6d0d833eefe2ab94bbeb05840efb2282cef1ee1d
tubi55 <tubi55@nate.com>
2022-10-04 12:03:15 +0900

    bg 이미지태그로 변경 및 나머지 패널 반복 슬라이딩

--- js/main.js
+++ js/main.js
@@ -4,34 +4,38 @@ const bgList = document.querySelector('.bg ul');
 const btnPrev = document.querySelector('.prev');
 const btnNext = document.querySelector('.next');
 const frames = [picList, txtList, bgList];
+const speed = 500;
 
 for (const el of frames) el.style.marginLeft = '-100%';
 
 btnNext.addEventListener('click', (e) => {
 	e.preventDefault();
 
-	new Anime(picList, {
-		prop: 'margin-left',
-		value: '-200%',
-		duration: 500,
-		callback: () => {
-			picList.append(picList.firstElementChild);
-			picList.style.marginLeft = '-100%';
-		},
-	});
+	for (const el of frames) {
+		new Anime(el, {
+			prop: 'margin-left',
+			value: '-200%',
+			duration: speed,
+			callback: () => {
+				el.append(el.firstElementChild);
+				el.style.marginLeft = '-100%';
+			},
+		});
+	}
 });
 
 btnPrev.addEventListener('click', (e) => {
 	e.preventDefault();
 
-	new Anime(picList, {
-		prop: 'margin-left',
-		value: '0%',
-		duration: 500,
-		callback: () => {
-			console.log(picList.lastElementChild);
-			picList.prepend(picList.lastElementChild);
-			picList.style.marginLeft = '-100%';
-		},
-	});
+	for (const el of frames) {
+		new Anime(el, {
+			prop: 'margin-left',
+			value: '0%',
+			duration: 500,
+			callback: () => {
+				el.prepend(el.lastElementChild);
+				el.style.marginLeft = '-100%';
+			},
+		});
+	}
 });
